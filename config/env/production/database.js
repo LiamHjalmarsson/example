df968e49c825d5a9f4d066955c396c1d0d175643
LiamHjalmarsson <Liam.hjalmarsson@hotmail.com>
2024-09-09 13:47:14 +0200

    configured

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,27 +1,3 @@
-// const parse = require("pg-connection-string").parse;
-
-// const { host, port, database, user, password } = parse(
-//     process.env.DATABASE_URL || ''
-// );
-
-// module.exports = ({ env }) => ({
-//     connection: {
-//         client: 'postgres',
-//         connection: {
-//             host,
-//             port,
-//             database,
-//             user,
-//             password,
-//             ssl: {
-//                 rejectUnauthorized: false,
-//             },
-//         },
-//         debug: false,
-//     },
-// });
-
-
 module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
